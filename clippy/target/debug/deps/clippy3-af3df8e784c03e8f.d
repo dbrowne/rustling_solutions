c2a40ac7f3113afd0ebc75e0f15eb435b3d1e86b
dbrowne <dwight@dwightjbrowne.com>
2022-11-21 12:10:42 -0800

    Initial Commit

---
+++ clippy/target/debug/deps/clippy3-af3df8e784c03e8f.d
@@ -0,0 +1,7 @@
+/mnt/source1/djbGR/ruststuffs/rustlings/exercises/clippy/target/debug/deps/clippy3-af3df8e784c03e8f.rmeta: clippy3.rs
+
+/mnt/source1/djbGR/ruststuffs/rustlings/exercises/clippy/target/debug/deps/clippy3-af3df8e784c03e8f.d: clippy3.rs
+
+clippy3.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
---
+++ generics/generics2.rs
@@ -0,0 +1,30 @@
+// This powerful wrapper provides the ability to store a positive integer value.
+// Rewrite it using generics so that it supports wrapping ANY type.
+
+// Execute `rustlings hint generics2` or use the `hint` watch subcommand for a hint.
+
+
+struct Wrapper <g>{
+    value: g,
+}
+
+impl <g> Wrapper<g> {
+    pub fn new(value: g) -> Self {
+        Wrapper { value }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn store_u32_in_wrapper() {
+        assert_eq!(Wrapper::new(42).value, 42);
+    }
+
+    #[test]
+    fn store_str_in_wrapper() {
+        assert_eq!(Wrapper::new("Foo").value, "Foo");
+    }
+}
